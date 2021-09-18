02724928e5d0e653ef0a33a4f89a113bd8351200
tripler <tripler@noreply.tripler.xyz>
2021-09-18 11:15:33 +0800

    init

---
+++ src/test/java/xyz/tripler/learningwebflux/LearningWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.tripler.learningwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
